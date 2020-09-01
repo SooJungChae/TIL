79bb3dafe02c7dcc1e0a6d7e4a26e094b1e6c28e
SoojungChae <naan_ace@naver.com>
2020-09-01 22:25:17 +0900

    ADD programmers dfs.js

---
+++ algorithm/programmers/dfs.js
@@ -0,0 +1,17 @@
+function solution(numbers, target) {
+    let answer = 0;
+    getAnswer(0,0);
+    function getAnswer(x,value) {
+        if(x<numbers.length){
+            getAnswer(x+1,value + numbers[x]);
+            getAnswer(x+1,value - numbers[x]);
+        } else{
+            if(value === target){
+                answer++
+            }
+        }
+    }
+    return answer;
+}
+
+solution([1,1,1,1,1], 5);
