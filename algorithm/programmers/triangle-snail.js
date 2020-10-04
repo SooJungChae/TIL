06dcc575e9fc1958ef14a3b56096d55797104054
soojeong <naan_ace@naver.com>
2020-10-04 16:50:00 +0900

    Working another way

--- algorithm/programmers/triangle-snail.js
+++ algorithm/programmers/triangle-snail.js
@@ -9,40 +9,59 @@ function roomLength (n) {
   return roomLength;
 }
 
+function getFloorIndexes(n) {
+  const result = [];
+  
+  let index = 0;
+  for (let i = 0; i < n; i++) {
+    index += i;
+    result.push(index);
+  }
+  
+  return result;
+}
+
 function solution (n) {
   const rLen = roomLength(n);
   const answer = new Array(rLen);
-  const floorIdx = rLen - n;
   
-  let leftPos = 0;
-  let rightPos = floorIdx - 1;
-  
-  let incStart = 1;
   let c = 1;
+  let startFloorIndex = 0;
+  let endFloorIndex = n - 1;
   
-  // draw floor
-  let inc = 0;
-  for (let i = floorIdx; i < rLen; i++) {
-    answer[i] = n + inc;
-    inc++;
+  let writeStartIndex = 0;
+  let writeEndIndex = n - 1;
+  
+  const floorIndexes = getFloorIndexes(n);
+  
+  // searchR();
+  // searchL();
+  
+  while (c <= rLen) {
+    searchR();
+    searchL();
   }
   
+  console.log(answer);
+  
   function searchR () {
-    leftPos = answer.findIndex(r => !r);
-    let idx = leftPos;
-    
-    while (idx < floorIdx) {
-      answer[idx] = c++;
-      console.log(answer);
-      idx += incStart;
-      
-      incStart++;
-      
-      // floor 는 다른 곳에서 ...
-      if (c === n) {
-        c += n;
+    for(let i = startFloorIndex; i <= endFloorIndex; i++) {
+      answer[floorIndexes[i] + writeStartIndex] = c++;
+  
+      // draw floor
+      if (i === endFloorIndex) {
+        for (let inc = 1; inc < n; inc++) {
+          answer[floorIndexes[i] + inc] = c++;
+          console.log(answer);
+        }
+        
+        endFloorIndex--;
+        // writeEndIndex--;
       }
     }
+    
+    writeStartIndex++;
+    startFloorIndex++;
   }
   
   function searchL () {
@@ -50,36 +69,17 @@ function solution (n) {
       return;
     }
     
-    for(let i = floorIdx - 1; i > leftPos; i--) {
-      if (!answer[i]) {
-        rightPos = i;
-        break;
-      }
-    }
-    
-    let idx = rightPos;
-    
-    while (idx > leftPos) {
-      incStart--;
+    for(let i = endFloorIndex; i >= startFloorIndex; i--) {
+      writeEndIndex--;
       
-      answer[idx] = c++;
+      answer[floorIndexes[i] + writeEndIndex] = c++;
       console.log(answer);
-  
-      idx -= incStart;
-      
     }
-  }
   
-  while (c <= rLen) {
-    searchR();
-    searchL();
-    
-    incStart++;
+    writeEndIndex = endFloorIndex;
   }
-  
-  return answer;
 }
 
 // console.log(solution(4), 'shoud be', [1,2,9,3,10,8,4,5,6,7]);
-// console.log(solution(5), 'shoud be', [1, 2, 12, 3, 13, 11, 4, 14, 15, 10, 5, 6, 7, 8, 9]);
-console.log(solution(6), 'shoud be', [1,2,15,3,16,14,4,17,21,13,5,18,19,20,12,6,7,8,9,10,11]);
+console.log(solution(5), 'shoud be', [1, 2, 12, 3, 13, 11, 4, 14, 15, 10, 5, 6, 7, 8, 9]);
+// console.log(solution(6), 'shoud be', [1,2,15,3,16,14,4,17,21,13,5,18,19,20,12,6,7,8,9,10,11]);
