39ffc6bfaa30f030225d12f9a891e31a8e8e0ffe
soojeong <naan_ace@naver.com>
2020-10-05 21:05:48 +0900

    Edit puzzle04
    - Use 10, 1 unit calc
    - Calc h,m,s seperately is more efficient

--- algorithm/javascript/puzzle04.js
+++ algorithm/javascript/puzzle04.js
@@ -2,61 +2,26 @@
 // 00:00:00 ~ 12:59:59 까지 구한다.
 function solution(segment) {
   const time = {};
-  const segCounts = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6];
-  const segmentTimes = [];
+  let cnt = 0;
   
-  // 00,00,00
-  // 00,00,01
-  
-  for(let h = 0; h < 13; h++) {
-    if (h - 10 < 0) {
-      h = '0' + h;
-    }
-    
+  for(let h = 0; h < 24; h++) {
     for(let m = 0; m < 60; m++) {
-      if (m - 10 < 0) {
-        m = '0' + m;
-      }
-      
       for(let s = 0; s < 60; s++) {
-        if (s - 10 < 0) {
-          s = '0' + s;
+        if (calc(h) + calc(m) + calc(s) === segment) {
+          cnt++;
         }
-  
-        console.log(s);
-        calc(h + '', m + '', s + '');
       }
     }
   }
   
-  function calc(h, m, s) {
-    if (!time[h]) {
-      time[h] = h.split('').reduce((acc, cur) => {
-        return acc += segCounts[cur] * 1;
-      }, 0);
-    }
-    
-    if (!time[m]) {
-      time[m] = m.split('').reduce((acc, cur) => {
-        return acc += segCounts[cur] * 1;
-      }, 0);
-    }
-  
-    if (!time[s]) {
-      console.log(s);
-      time[s] = s.split('').reduce((acc, cur) => {
-        return acc += segCounts[cur] * 1;
-      }, 0);
-    }
-  
-    let totalCalc = time[h] + time[m] + time[s];
-  
-    if (totalCalc === segment) {
-      segmentTimes.push(`${h}:${m}:${s}`);
-    }
-  }
-  
-  return segmentTimes.length;
+  return cnt;
 }
 
-solution(30);
+function calc(t) {
+  const light = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6];
+  
+  // 10의 자리, 1의 자리를 각각 더해준다.
+  return light[Math.floor(t / 10)] + light[Math.floor(t % 10)];
+}
+
+console.log(solution(30));
