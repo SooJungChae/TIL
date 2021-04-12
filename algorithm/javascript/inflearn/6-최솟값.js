831b358bb4d5119dcfab74b53f1f76ed7f48ae1d
ChaeSooJung <naan_ace@naver.com>
2021-04-12 14:57:34 +0900

    Add inflearn algorithm min, max method

--- algorithm/javascript/inflearn/6-최솟값.js
+++ algorithm/javascript/inflearn/6-최솟값.js
@@ -14,3 +14,9 @@ console.log(solution([5, 3, 7, 11, 2, 15, 17]));
 
 // for 문 쓸 땐 arr[0] 으로 바로 접근할 수도 있고,
 // 처음부터 돌고싶다면 Number.MAX_SAFE_INTEGER 쓰면 된다.
+
+// 최소, 최댓값 구하는 내장함수가 있다.
+// Math.min(...arr) / Math.max(...arr)
+
+// 전개연산자 안쓰려면
+// Math.min.apply(null, arr) / Math.max.apply(null, arr)
