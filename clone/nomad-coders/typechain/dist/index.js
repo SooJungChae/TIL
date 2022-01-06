f46d0669c4c86b1fe910539a8a7377f0fa8d57f7
SooJungChae <naan_ace@naver.com>
2022-01-06 23:10:02 +0900

    Ignore dist folder

--- clone/nomad-coders/typechain/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sayHello = (name, age) => {
-    console.log(`Hello ${name}, you are ${age}!`);
-};
-sayHello('soo', 24);
-//# sourceMappingURL=index.js.map
