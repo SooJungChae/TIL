a32665a69809264aef5caf2b543ad9e0e80b7861
Soojung Chae <naan_ace@naver.com>
2022-01-09 20:43:52 +0900

    [7, 8] Add blockchain type, functions (#80)
    * [7] Add block type and blockchain type.
    
    * [8] Add crypto-js package
    
    * [8] Add block static function and others

--- clone/nomad-coders/typechain/src/index.ts
+++ clone/nomad-coders/typechain/src/index.ts
@@ -1,47 +1,43 @@
-// interface 테스트하고 싶으면 주석 해제, class 부분은 주석 처리.
-// // interface
-// interface Human {
-//   name: string;
-//   age: number;
-//   gender?: string;
-// }
-//
-// const person: Human = {
-//   name: 'soo',
-//   age: 24,
-//   gender: 'woman'
-// }
-//
-// const sayHello = (person: Human): void => {
-//   console.log(`Hello ${person.name}, you are ${person.age}!`);
-// }
-//
-// sayHello(person);
-//
-// export {};
+import * as CryptoJS from "crypto-js";
 
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
 
-// class
-class Human {
-  // ts 에서는 속성을 선언해줘야 한다.
-  public name: string;
-  private age: number;
-  public gender?: string;
+  // static 를 써야 new Block 을 안하더라도 Block.calculateBlockHash 접근이 가능하다.
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ): string => CryptoJS.SHA256(index + previousHash + data + timestamp);
 
-  // class 가 시작할 때마다 호출된다. (객체를 만들 때)
-  constructor(name: string, age: number, gender?: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const person = new Human('soo', 24);
+const genesisBlock: Block = new Block(0, "20202020202020", "", "Hello", 12345);
 
-const sayHello = (person: Human): void => {
-  console.log(`Hello ${person.name}, you are ${person.age}!`);
-}
+let blockchain: Block[] = [genesisBlock];
 
-sayHello(person);
+const getBlockHain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+console.log(blockchain);
 
 export {};
