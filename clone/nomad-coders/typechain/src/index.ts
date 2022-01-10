c32ec37a0c3dcacaeb9f046e1b8863234e9a6330
Soojung Chae <naan_ace@naver.com>
2022-01-10 20:15:00 +0900

    [9, 10] Add createNewBlock, block validation functions (#81)
    * [9] Add create new block function
    
    * [10] Add block validation function

--- clone/nomad-coders/typechain/src/index.ts
+++ clone/nomad-coders/typechain/src/index.ts
@@ -15,6 +15,17 @@ class Block {
     timestamp: number
   ): string => CryptoJS.SHA256(index + previousHash + data + timestamp);
 
+  // 블록이 유효한지 검증한다.
+  static validateStructure = (block: Block): boolean => {
+    return (
+      typeof block.index === "number" &&
+      typeof block.hash === "string" &&
+      typeof block.previousHash === "string" &&
+      typeof block.data === "string" &&
+      typeof block.timestamp === "number"
+    );
+  };
+
   constructor(
     index: number,
     hash: string,
@@ -34,10 +45,46 @@ const genesisBlock: Block = new Block(0, "20202020202020", "", "Hello", 12345);
 
 let blockchain: Block[] = [genesisBlock];
 
-const getBlockHain = (): Block[] => blockchain;
+const getBlockchain = (): Block[] => blockchain;
 
 const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
-console.log(blockchain);
+const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const latestBlock: Block = getLatestBlock();
+  const newIndex: number = latestBlock.index + 1;
+  const newTimestamp: number = getNewTimestamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    latestBlock.previousHash,
+    data,
+    newTimestamp
+  );
+  const newBlock = new Block(
+    newIndex,
+    newHash,
+    latestBlock.hash,
+    data,
+    newTimestamp
+  );
+  return newBlock;
+};
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  }
+  return true;
+};
+
+// console.log(blockchain);
+
+console.log(createNewBlock("first block"));
+console.log(createNewBlock("second block"));
 
 export {};
